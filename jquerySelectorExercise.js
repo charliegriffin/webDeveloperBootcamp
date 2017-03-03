7a29327ade67e5a688becc5d0b392b4bd28d06b4
Charlie Griffin <charlesgriffin@Charless-MacBook-Pro.local>
2017-03-03 09:56:52 -0600

    Some basic jQuery exercises
    I’m >50% finished with the course!

---
+++ jquerySelectorExercise.js
@@ -0,0 +1,11 @@
+// Select all divs and give them a purple background
+$("div").css("background", "purple");
+
+// Select the divs with class "highlight" and make them 200px wide
+$("div.highlight").css("width","200px");
+
+// Select the div with the id "third" and give it an orange border
+$("#third").css("border","4px solid orange");
+
+// Bonus: Select first div only and change the font color to pink
+$("div:first-of-type").css("color","pink");
